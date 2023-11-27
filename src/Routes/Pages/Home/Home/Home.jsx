631b111308d4293ab096e89148599ff66be1b91d
jihaduddinjbm <jihaduddin117@gmail.com>
2023-11-28 01:24:51 +0600

    added new created jsx file

--- src/Routes/Pages/Home/Home/Home.jsx
+++ src/Routes/Pages/Home/Home/Home.jsx
@@ -1,9 +1,13 @@
+import About from "../About/About";
+import Banner from "../Banner/Banner";
 
 
 const Home = () => {
     return (
         <div>
-            <h2> This is Home</h2>
+            <Banner></Banner>
+            <About></About>
+            
         </div>
     );
 };
