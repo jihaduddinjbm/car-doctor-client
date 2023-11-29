d86862b827490d880d93b8066e40b8e6b4c85019
jihaduddinjbm <jihaduddin117@gmail.com>
2023-11-29 10:06:43 +0600

    added new created jsx file

---
+++ src/Routes/Pages/Home/Services/ServiceCard.jsx
@@ -0,0 +1,21 @@
+
+
+const ServiceCard = ({service}) => {
+    const {_id, service_id, title, img, price, description} = service;
+    return (
+        <div className="card w-96 bg-base-100 shadow-xl">
+  <figure className="px-10 pt-10">
+    <img src={img} alt="Shoes" className="rounded-xl" />
+  </figure>
+  <div className="card-body items-center text-center">
+    <h2 className="card-title text-2xl text-white">{title}</h2>
+    <p className="text-xl text-white"> price $: {price}</p>
+    <div className="card-actions">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default ServiceCard;
